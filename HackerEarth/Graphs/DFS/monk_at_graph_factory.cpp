0103e63b35b0c52e4d7d839cb1449008036fc99e
veedee <43088920+veedee2000@users.noreply.github.com>
2019-09-28 13:12:59 +0530

    Create monk_at_graph_factory.cpp
    https://www.hackerearth.com/practice/algorithms/graphs/graph-representation/practice-problems/algorithm/monk-at-the-graph-factory/

---
+++ HackerEarth/Graphs/DFS/monk_at_graph_factory.cpp
@@ -0,0 +1,30 @@
+// I am Varun Das from Jadavpur University
+#include <bits/stdc++.h>
+#pragma GCC target ("avx2")
+#pragma GCC optimization ("O3")
+#pragma GCC optimization ("unroll-loops")
+#define mod 1000000007
+#define ll long long
+#define dbg(x) cerr << #x << " = " << x << endl
+#define INF LLONG_MAX
+#define loop(i,a,b) for(int i=a;i<b;i++)
+#define rep(i,a,b) for(int i=a;i>=b;i--)
+#define nax 503405
+using namespace std;
+ 
+int main() {
+#ifdef _DEBUG
+	freopen("input.txt", "r", stdin);
+//	freopen("output.txt", "w", stdout);
+#endif
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+//cout<<fixed<<setprecision(12);
+ll n,x,v=0;
+cin>>n;
+loop(i,0,n) cin>>x,v+=x;
+if(v==2*(n-1)) cout<<"Yes";
+else cout<<"No";
+ 
+	return 0;
+}

909db316d70a4e8fc09db9e8204d47fb5a9584ab
veedee <43088920+veedee2000@users.noreply.github.com>
2019-10-18 20:30:14 +0530

    Update Power_of_Two.cpp

--- Leetcode/BIT_MANIPULATION/Power_of_Two.cpp
+++ Leetcode/BIT_MANIPULATION/Power_of_Two.cpp
@@ -1,9 +1,12 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
+        int c = 0;
+        if(n < 1) return false;
         for(int i = 31; i>=0;i--){
-            if((1<<i) & n) return true;
+            if((1<<i) & n) c++;
         }
+        if(c == 1) return true;
         return false;
     }
 };

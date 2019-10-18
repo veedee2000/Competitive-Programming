5bd669744236bb47da759a2c7b8b9f19152811ef
veedee <43088920+veedee2000@users.noreply.github.com>
2019-10-18 20:24:42 +0530

    Create Reverse_Bits.cpp

---
+++ Leetcode/BIT_MANIPULATION/Reverse_Bits.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t ans=0;
+        for(int i=0;i<32;i++){
+            if((1<<i) & n) ans += 1<<(31 - i);
+        }
+        return ans;
+    }
+};

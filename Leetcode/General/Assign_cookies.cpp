314bb0f66ee56fa46ed6d4e2d20e96605026b193
veedee <43088920+veedee2000@users.noreply.github.com>
2019-10-19 02:01:47 +0530

    Create Assign_cookies.cpp

---
+++ Leetcode/General/Assign_cookies.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+        int i = 0,j = 0,ans = 0;
+        while(i < g.size() and j < s.size()){
+            if(s[j] >= g[i]) {++ans; i++; j++;}
+            else if(s[j] < g[i]) {j++;}
+        }
+        return ans;
+    }
+};

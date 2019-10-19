0ee3c1a01c495e68a2093ba71b7ad5e3d9a3b6ad
veedee <43088920+veedee2000@users.noreply.github.com>
2019-10-19 16:20:27 +0530

    Update 4Sum_2(Binary_Search_NW).cpp

--- Leetcode/General/4Sum_2(Binary_Search_NW).cpp
+++ Leetcode/General/4Sum_2(Binary_Search_NW).cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
     int fourSumCount(vector<int>& A, vector<int>& B, vector<int>& C, vector<int>& D) {
         sort(D.begin(),D.end());
+        map<int,int>mp;
+        for(auto x:D) mp[x]++;
         int ans = 0;
         for(auto a:A){
             for(auto b:B){
@@ -16,12 +18,13 @@ public:
                     while(l <= r-1){
                         m = l + (r - l)/2;
                         if(l == r-1){
-                            if(sum == D[l] or sum == D[r]) ++ans;
+                            if(sum == D[l]) ans += mp[D[l]];
+                            else if(sum == D[r]) ans += mp[D[r]];
                             break;
                         }
                         else{
                             if(sum == D[m]){
-                                {++ans; break;}
+                                {ans += mp[D[m]]; break;}
                             }
                             else if(sum < D[m]){
                                 r = m;

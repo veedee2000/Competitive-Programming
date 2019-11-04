e11bd4b6d187d15e76050851f1f4ae2f4c8f197d
veedee <43088920+veedee2000@users.noreply.github.com>
2019-11-05 02:28:52 +0530

    Update Number_Palindromic_Substrings(Basic_Method).cpp

--- Leetcode/String-Manipulation/Number_Palindromic_Substrings(Basic_Method).cpp
+++ Leetcode/String-Manipulation/Number_Palindromic_Substrings(Basic_Method).cpp
@@ -5,28 +5,22 @@ public:
         int n = s.size();
         if(n == 0) return 0;
         int val = 0;
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n;i++){     // For Checking odd length Palindromes
             
-            string ans = "";
-            ans += s[i];
             int l = i, r = i;
-            ++val;
+            ++val;               // Since single characters are also palindromes
             while(l > 0 and r < n-1 and s[--l] == s[++r]){
-                ans = s[l] + ans + s[r];
                 ++val;
             }
-        }
-        
-        for(int i=0;i<n-1;i++){
             
-            string ans = "";
+        }
+         
+        for(int i=0;i<n-1;i++){    // For Checking even length palindromes
+            
             int l = i, r = i+1;
-            if(s[l] != s[r]) continue;
-            ++val;
-            ans += s[l];
-            ans += s[r];
+            if(s[l] != s[r]) continue;   
+            ++val;                // For 2 character palindromes
             while(l > 0 and r < n-1 and s[--l] == s[++r]){
-                ans = s[l] + ans + s[r];
                 ++val;
             }
 

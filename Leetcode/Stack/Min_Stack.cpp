3014609deb25e4b98d7bcc717d5fcd4c96d43483
veedee <43088920+veedee2000@users.noreply.github.com>
2020-02-01 15:11:14 +0530

    Create Min_Stack.cpp

---
+++ Leetcode/Stack/Min_Stack.cpp
@@ -0,0 +1,38 @@
+class MinStack {
+public:
+    /** initialize your data structure here. */
+    stack<int>s,min;
+    MinStack() {
+        
+    }
+    
+    void push(int x) {
+        if(min.empty()) min.push(x);
+        else{
+            if(x <= min.top()) min.push(x);
+        }
+        s.push(x);
+    }
+    
+    void pop() {
+        if(s.top() == min.top()) s.pop(),min.pop();
+        else s.pop();
+    }
+    
+    int top() {
+        return s.top();
+    }
+    
+    int getMin() {
+        return min.top();
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(x);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */

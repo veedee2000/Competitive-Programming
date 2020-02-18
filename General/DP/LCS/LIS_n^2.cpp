8adf0bce459f17888cfc4823c5a651f06a14f496
veedee <43088920+veedee2000@users.noreply.github.com>
2020-02-19 02:11:43 +0530

    Update LIS_n^2.cpp

--- General/DP/LCS/LIS_n^2.cpp
+++ General/DP/LCS/LIS_n^2.cpp
@@ -1,18 +1,26 @@
 class Solution {
 public:
-    vector<int>dp;
-    int findNumberOfLIS(vector<int>& nums) {
+    vector<int>dp,nums;
+    int lengthOfLIS(vector<int>& nums) {
+        this -> nums = nums;
         dp.resize(nums.size(),-1);
-        return f(nums.size() - 1,nums);
+        int ans = 0;
+        for(int i = nums.size() - 1;i >= 0;i--){
+            ans = max(ans,f(i));
+        }
+        return ans;
     }
     
-    int f(int i,vector<int>& nums){
-        if(i < 0) return 0;
+    int f(int i){
         if(dp[i] != -1) return dp[i];
         int ans = 0;
-        for(int j = 0;j < i;j++){
-            if(nums[i] > nums[j]) ans = max(ans,f(j,nums));
+        for(int j = i - 1;j >= 0;j--){
+            if(nums[j] < nums[i]) ans = max(ans,f(j));
         }
         return dp[i] = ans + 1;
     }
+    
 };
+
+// Total Number of Operations performed = k1 * n ^ 2 + k2 * n ^ 2.
+// Complexity = O (n ^ 2).

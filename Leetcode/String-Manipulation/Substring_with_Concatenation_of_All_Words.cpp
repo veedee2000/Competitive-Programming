dcc3f87ef0d1ccbbe51326479e727d9d5ca4df42
veedee <43088920+veedee2000@users.noreply.github.com>
2020-02-23 03:27:48 +0530

    Create Substring_with_Concatenation_of_All_Words.cpp

---
+++ Leetcode/String-Manipulation/Substring_with_Concatenation_of_All_Words.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    unordered_map<string,int>mp;
+    int sz;
+    vector<int> findSubstring(string s, vector<string>& words) {
+        vector<int>ans;
+        if(words.size() == 0) return {};
+        sz = words[0].size();
+        int totalSize = sz * words.size();
+        for(auto x:words) mp[x]++;
+        string v;
+        for(int i = 0;i < s.size();i++){
+            v.push_back(s[i]);
+            if(i < totalSize - 1) continue;
+            if(check(v)) ans.push_back(i - totalSize + 1);
+            v.erase(0,1);
+        }
+        return ans;
+    }
+    
+    bool check(string v){
+        unordered_map<string,int>sub;
+        for(auto x:mp) sub[x.first] = x.second;
+        for(int i = 0;i < v.size();i += sz){
+            if(!sub.count(v.substr(i,sz)) or (--sub[v.substr(i,sz)] < 0)) return 0;
+        }
+        return 1;                                
+    }
+};

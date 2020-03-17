ed17622f320adc97942de7107bade398b92a3f77
veedee <43088920+veedee2000@users.noreply.github.com>
2020-03-17 21:31:12 +0530

    Update Network_Time_Delay.cpp

--- Leetcode/Graph_Theory/Shortest_Path_Algorithms/Dijkstra/Network_Time_Delay.cpp
+++ Leetcode/Graph_Theory/Shortest_Path_Algorithms/Dijkstra/Network_Time_Delay.cpp
@@ -1,46 +1,41 @@
-#define INF 6000000
 class Solution {
-public:
-    vector<vector<pair<int,int>>>adj;
-    vector<int>d,p;
-    int N,ans = 0;
-    int networkDelayTime(vector<vector<int>>& times, int N, int K) {
-        adj.resize(N + 1);
-        for(auto x:times){
-            adj[x[0]].push_back({x[1],x[2]});
-        }
-        this -> N = N;
-        d.resize(N + 1, INF);
-        p.resize(N + 1, -1);
-        dijkstra(K);
-        for(int i = 1;i <= N;i++){
-            if(d[i] == INF) return -1;
-            ans = max(ans,d[i]);
-        }
-        return ans;
-    }
     
-    void dijkstra(int K){
-        vector<bool>u(N + 1,0);
-        d[K] = 0;
-        for(int i = 1;i <= N;i++){
-            int v = -1;
-            for(int j = 1;j <= N;j++){
-                if(!u[j] and (v == -1 or d[j] < d[v])) v = j;
-            }
-            if(d[v] == INF) break;
-
-            u[v] = 1;
-            for(auto edge : adj[v]){
-                int to = edge.first;
-                int len = edge.second;
-
-                if(d[v] + len < d[to]){
-                    d[to] = d[v] + len;
-                    p[to] = v;
+    struct comp{
+        bool operator()(vector<int>&a,vector<int>&b){
+            return a[1] > b[1];
+        }
+    };
+    
+public:
+    int networkDelayTime(vector<vector<int>>& times, int N, int K) {
+        vector<int>dist(N + 1,INT_MAX);
+        dist[0] = 0;
+        dist[K] = 0;
+        vector<vector<vector<int>>>v(N + 1);
+        for(auto x:times){
+            v[x[0]].push_back({x[1],x[2]});
+        }
+        priority_queue<vector<int>,vector<vector<int>>,comp>pq;
+        pq.push({K,0});
+        vector<bool>b(N + 1,0);
+        while(!pq.empty()){
+            vector<int>curr = pq.top();
+            pq.pop();
+            b[curr[0]] = 1;
+            for(auto x:v[curr[0]]){ 
+                if(!b[x[0]]){
+                    if(curr[1] + x[1] < dist[x[0]]){
+                        dist[x[0]] = curr[1] + x[1];
+                        pq.push({x[0],dist[x[0]]});
+                    }
                 }
             }
         }
+        int ans = 0;
+        for(auto x:dist){
+            if(x == INT_MAX) return -1;
+            ans = max(ans,x);
+        }
+        return ans;
     }
-    
 };

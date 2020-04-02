5ea16ec649747bf5c6451964d06f47164e07c903
veedee <43088920+veedee2000@users.noreply.github.com>
2020-04-02 18:55:16 +0530

    Update Reorganize_String.cpp

--- Leetcode/Heaps/Reorganize_String.cpp
+++ Leetcode/Heaps/Reorganize_String.cpp
@@ -1,36 +1,36 @@
 class Solution {
+    
+    struct comp{
+        bool operator()(pair<char,int>a,pair<char,int>b){
+            return a.second < b.second;
+        }
+    };
+    
+    const int INF = 1000;
+    
 public:
-    string reorganizeString(string S) {
-        auto comp = [](pair<char,int> a,pair<char,int> b){
-            return (a.second < b.second);
-        };
-        priority_queue<pair<char,int>,vector<pair<char,int>>,decltype(comp)>q(comp);
+    string reorganizeString(string S){
+        if(S == "") return "";
         unordered_map<char,int>mp;
         for(auto x:S) mp[x]++;
+        priority_queue<pair<char,int>,vector<pair<char,int>>,comp>pq;
         for(auto x:mp){
-            q.push({x.first,x.second});
+            pq.push(x);
         }
-        string ans = "";
-        pair<int,int>a = q.top();
-        q.pop();
-        a.second += 1000;
-        q.push(a);
-        while(!q.empty()){
-            pair<int,int>curr = q.top();
-            q.pop();
-            ans.push_back(curr.first);
-            if(!q.empty()){
-                pair<char,int>topNow = q.top();
-                q.pop();
-                topNow.second += 1000;
-                q.push(topNow);
+        string ans;
+        while(!pq.empty()){
+            pair<char,int>top = pq.top();
+            pq.pop();
+            ans.push_back(top.first);
+            if(pq.empty()){
+                if(top.second == 1) break;
+                else return "";
             }
-            if(curr.second - 1000 > 1) q.push({curr.first,curr.second - 1001});
-        }
-        if(ans.size() == 1) return ans;
-        cout<<ans;
-        for(int i = 1;i < ans.size();i++){
-            if(ans[i] == ans[i - 1]) return "";
+            pair<char,int>secondTop = pq.top();
+            pq.pop();
+            ans.push_back(secondTop.first);
+            if(--top.second) pq.push(top);
+            if(--secondTop.second) pq.push(secondTop);
         }
         return ans;
     }

680ef75b7dae41275bcdd6d81a16e6beb7c55e0a
veedee <43088920+veedee2000@users.noreply.github.com>
2020-04-03 06:07:21 +0530

    Create Sum_of_Noes_With_Even_Valued_Grandparent.cpp

---
+++ Leetcode/BST/Sum_of_Noes_With_Even_Valued_Grandparent.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+    /*
+        Given node -> none - 0
+        Given node -> child - 1
+        Given node -> grandchild - 2
+        Given node -> both - 3
+    */
+public:
+    int ans;
+    int sumEvenGrandparent(TreeNode* root) {
+        ans = 0;
+        f(root,0);
+        return ans;
+    }
+    
+    void f(TreeNode* root,int c){
+        if(!root) return;
+        if(c < 2){
+            ans += c ? root -> val : 0;
+            if(root -> val % 2 == 0) f(root -> right,2),f(root -> left,2);
+            else f(root -> right,0),f(root -> left,0);
+        }
+        else if(c == 2){
+            if(root -> val % 2 == 0) f(root -> right,3),f(root -> left,3);
+            else f(root -> right,1),f(root -> left,1);
+        }
+        else{
+            ans += root -> val;
+            if(root -> val % 2 == 0) f(root -> right,3),f(root -> left,3);
+            else f(root -> right,1),f(root -> left,1);
+        }
+    }
+};

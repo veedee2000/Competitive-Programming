695b3927003066dfd6782cee78619b194eba5914
veedee <43088920+veedee2000@users.noreply.github.com>
2020-04-06 22:02:19 +0530

    Create Combination_Sum.cpp

---
+++ Leetcode/BackTracking/Combination_Sum.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>>ans;
+    vector<int>candidates;
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        sort(candidates.begin(),candidates.end());
+        this -> candidates = candidates;
+        f(0,target,{});
+        return ans;
+    }
+    
+    void f(int i,int target,vector<int>path){
+        if(target == 0){
+            ans.push_back(path);
+            return;
+        }
+        for(int j = i;j < candidates.size() and candidates[j] <= target;j++){
+            path.push_back(candidates[j]);
+            f(j,target - candidates[j],path);
+            path.pop_back();
+        }
+    }
+};

35fc17a1fb6a7b49916eb4dab4f4994122ecde4c
veedee <43088920+veedee2000@users.noreply.github.com>
2020-04-07 10:20:34 +0530

    Update fenwick.cpp

--- General/Trees/FENWICK/fenwick.cpp
+++ General/Trees/FENWICK/fenwick.cpp
@@ -1,1 +1,41 @@
-
+ class NumArray {
+    private:
+        vector<int>nums,BIT;
+      
+        int query(int i){
+            i++;
+            int sum = 0;
+            while(i > 0){
+                sum += BIT[i];
+                i -= i&-i;
+            }
+            return sum;
+        }
+        
+        void add(int i, int val){
+            i++;
+            while(i < BIT.size()){
+                BIT[i]+=val;
+                i += i&-i; 
+            }
+        }
+        
+    public:
+        NumArray(vector<int> &nums) : nums(nums) {
+            BIT.resize(nums.size()+1);
+            for(int i = 0;i < nums.size();i++){
+                add(i,nums[i]);
+            }
+        }
+    
+        void update(int i,int val) {
+            if(val != nums[i]){
+                add(i,val - nums[i]);
+                nums[i]=val;
+            }
+        }
+    
+        int sumRange(int i, int j) {
+            return query(j) - query(i - 1);
+        }
+    };

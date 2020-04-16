73db8723d9e401eeef7ae07c1b1d66efad6da1c5
veedee <43088920+veedee2000@users.noreply.github.com>
2020-04-16 20:35:49 +0530

    Update Find_Minimum_Rotated_Sorted_Array.cpp

--- Leetcode/Tricky_Problems/Find_Minimum_Rotated_Sorted_Array.cpp
+++ Leetcode/Tricky_Problems/Find_Minimum_Rotated_Sorted_Array.cpp
@@ -4,9 +4,8 @@ public:
         int l = 0,r = nums.size() - 1,m;
         if(nums.size() == 1) return nums[0];
         if(nums[l] < nums[r]) return nums[0];
-        while(l < r){
+        while(l <= r){
             m = l + (r - l) / 2;
-            if(nums[m] > nums[m + 1]) return nums[m + 1];
             if(m > 0 and nums[m - 1] > nums[m]) return nums[m];
             if(m < nums.size() - 1 and nums[m] > nums[m + 1]) return nums[m + 1];
             if(nums[m] > nums[l] and nums[m] > nums[r]) l = m + 1;

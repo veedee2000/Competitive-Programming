393ca29222149cc3baae0041a4dd31273af5a332
veedee <43088920+veedee2000@users.noreply.github.com>
2020-04-18 11:54:51 +0530

    Update Kth_Smallest_Element.cpp

--- Leetcode/Heaps/Kth_Smallest_Element.cpp
+++ Leetcode/Heaps/Kth_Smallest_Element.cpp
@@ -1,13 +1,23 @@
 class Solution {
+    
+    struct comp{
+        bool operator ()(vector<int>a,vector<int>b){
+            return a[0] > b[0];
+        }
+    };
+    
 public:
     int kthSmallest(vector<vector<int>>& matrix, int k) {
-        priority_queue<int,vector<int>>heap;
-        for(auto x:matrix){
-            for(auto y:x){
-                heap.push(y);
-                if(heap.size() > k) heap.pop();
-            }
+        int n = matrix.size(),ans;
+        priority_queue<vector<int>,vector<vector<int>>,comp>pq;
+        for(int i = 0;i < n;i++) pq.push({matrix[0][i],0,i});
+        while(k--){
+            vector<int>curr = pq.top();
+            ans = curr[0];
+            pq.pop();
+            int x = curr[1],y = curr[2];
+            if(x < n - 1) pq.push({matrix[x + 1][y],x + 1,y});
         }
-        return heap.top();
+        return ans;
     }
 };

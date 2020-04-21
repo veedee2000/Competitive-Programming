24d043ded18012a96ea426805fd25d2b7202d237
veedee <43088920+veedee2000@users.noreply.github.com>
2020-04-21 11:29:57 +0530

    Create LCA_in_BST.cpp

---
+++ Leetcode/BST/LCA_in_BST.cpp
@@ -0,0 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(!root) return NULL;
+        if(p -> val > q -> val) return lowestCommonAncestor(root, q, p);
+        if(root -> val < p -> val) return lowestCommonAncestor(root -> right, p, q);
+        if(root -> val > q -> val) return lowestCommonAncestor(root -> left, p, q);
+        return root;
+    }
+};

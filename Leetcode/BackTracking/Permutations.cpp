65f280c69d308ae7d4b27b8c93ed431854478a5c
veedee <43088920+veedee2000@users.noreply.github.com>
2020-04-22 22:27:00 +0530

    Update Permutations.cpp

--- Leetcode/BackTracking/Permutations.cpp
+++ Leetcode/BackTracking/Permutations.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
     vector<vector<int>>ans;
-    vector<vector<int>> permute(vector<int>& nums) {
-        f(0,{},nums);
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        vector<int>path = {};
+        f(nums,path);
         return ans;
     }
     
-    void f(int hash,vector<int>v,vector<int>& nums){
-        if(v.size() == nums.size()){
-            ans.push_back(v);
-            return;
-        }
-        
+    void f(vector<int>& nums,vector<int>& path){
+        if(!nums.size()) { ans.push_back(path); return; }
         for(int i = 0;i < nums.size();i++){
-            if((hash & (1 << i)) == 0){
-                v.push_back(nums[i]);
-                f(hash | (1 << i),v,nums);
-                v.pop_back();
-            }
+            int val = nums[i];
+            path.push_back(val);
+            nums.erase(nums.begin() + i);
+            f(nums,path);
+            nums.insert(nums.begin() + i,val);
+            path.pop_back();
         }
     }
-    
 };

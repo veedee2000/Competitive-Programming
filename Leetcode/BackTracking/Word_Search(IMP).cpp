56b838775a722becbb9f466cbf8f7e8f9f72db88
veedee <43088920+veedee2000@users.noreply.github.com>
2020-04-26 09:18:37 +0530

    Update Word_Search(IMP).cpp

--- Leetcode/BackTracking/Word_Search(IMP).cpp
+++ Leetcode/BackTracking/Word_Search(IMP).cpp
@@ -5,26 +5,24 @@ public:
     bool exist(vector<vector<char>>& board, string word) {
         m = board.size(),n = board[0].size();
         dr = {-1,1,0,0},dc = {0,0,-1,1};
-        vector<vector<bool>>b;
-        b.resize(m,vector<bool>(n,0));
-        for(int i = 0;i < board.size();i++){
-            for(int j = 0;j < board[0].size();j++){
-                if(f(i,j,0,board,word,b)) return 1;
+        for(int i = 0;i < m;i++){
+            for(int j = 0;j < n;j++){
+                if(f(i,j,0,board,word)) return 1;
             }
         }
         return 0;
     }
     
-    bool f(int i,int j,int pos,vector<vector<char>>& board, string& word,vector<vector<bool>>& b){
+    bool f(int i,int j,int pos,vector<vector<char>>& board, string& word){
         if(pos == word.size()) return 1;
         if(i < 0 or i >= m or j < 0 or j >= n) return 0;
         if(board[i][j] != word[pos]) return 0;
-        if(b[i][j]) return 0;
-        b[i][j] = 1;
+        char ch = board[i][j];
+        board[i][j] = '#';
         for(int rc = 0;rc < 4;rc++){
-            if(f(i + dr[rc],j + dc[rc],pos + 1,board,word,b)) return 1;
+            if(f(i + dr[rc],j + dc[rc],pos + 1,board,word)) return 1;
         }
-        b[i][j] = 0;
+        board[i][j] = ch;
         return 0;
     }
 };

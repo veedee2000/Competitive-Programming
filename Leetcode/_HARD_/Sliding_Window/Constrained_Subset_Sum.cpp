ef96ea6dedbd72e9ded89d35a2fe945889f8dd1f
veedee <43088920+veedee2000@users.noreply.github.com>
2020-04-27 10:06:00 +0530

    Update Constrained_Subset_Sum.cpp

--- Leetcode/_HARD_/Sliding_Window/Constrained_Subset_Sum.cpp
+++ Leetcode/_HARD_/Sliding_Window/Constrained_Subset_Sum.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     
-    int constrainedSubsetSum(vector<int>& nums, int k) {
+    int constrainedSubsetSum(vector<int>& dp, int k) {
         deque<pair<int,int>>d;
-        vector<int>dp = nums;
-        for(int i = 0;i < nums.size();i++){
+        for(int i = 0;i < dp.size();i++){
             dp[i] += d.size() ? max(d.back().first,0) : 0;
             while(d.size() and d.front().first < dp[i]) d.pop_front();
             d.push_front({dp[i],i});
             if(d.back().second == i - k) d.pop_back();
         }
-        int ans = nums[0];
+        int ans = dp[0];
         for(auto x:dp) ans = max(ans,x);
         return ans;
     }

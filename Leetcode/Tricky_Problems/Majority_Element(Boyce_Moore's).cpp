5279a649b237d62c6d80bad32e1084d454d736e1
veedee <43088920+veedee2000@users.noreply.github.com>
2020-05-10 16:50:26 +0530

    Create Majority_Element(Boyce_Moore's).cpp

---
+++ Leetcode/Tricky_Problems/Majority_Element(Boyce_Moore's).cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int major = nums[0],count = 1;
+        for(int i = 1;i < nums.size();i++){
+            if(count == 0){
+                count = 1;
+                major = nums[i];
+            }
+            else if(major == nums[i]) count++;
+            else count--;
+        }
+        return major
+    }
+};

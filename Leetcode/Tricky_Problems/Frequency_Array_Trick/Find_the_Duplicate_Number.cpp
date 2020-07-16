64ddf1861768ce4a66b6c26dbe79414b34b3862f
veedee <43088920+veedee2000@users.noreply.github.com>
2020-07-16 18:53:53 +0530

    Create Find_the_Duplicate_Number.cpp

---
+++ Leetcode/Tricky_Problems/Frequency_Array_Trick/Find_the_Duplicate_Number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        for(int i = 0;i < n;i++){
+            if(nums[nums[i] % n] > n) return nums[i] % n;
+            nums[nums[i] % n] += n;
+        }
+        return 0;
+    }
+};

770f1e9429a38cb12c90f86b42a1fce206bdf512
veedee <43088920+veedee2000@users.noreply.github.com>
2020-08-09 08:03:55 +0530

    Update Swim_in_Rising_Water.cpp

--- Leetcode/_HARD_/Heap/Swim_in_Rising_Water.cpp
+++ Leetcode/_HARD_/Heap/Swim_in_Rising_Water.cpp
@@ -1,26 +1,34 @@
+#define t tuple<int, int, int>
+
 class Solution {
+    
+    struct comp{
+        bool operator()(t& a, t& b){
+            return get<0>(a) > get<0>(b);
+        }
+    };
+    
 public:
     int swimInWater(vector<vector<int>>& grid) {
-        int n = grid.size(),ans = INT_MIN;
-        vector<bool>vis(n * n,0);
-        priority_queue<pair<int,int>>heap;
-        vector<int>dr = {-1,1,0,0},dc = {0,0,1,-1};
-        heap.push({-grid[0][0],0});
-        while(!heap.empty()){
-            int x = heap.top().second / n;
-            int y = heap.top().second % n;
-            heap.pop();
-            ans = max(ans,grid[x][y]);
-            if(x == n - 1 and y == n - 1) break;
-            for(int i = 0;i < 4;i++){
-                int xNew = x + dr[i];
-                int yNew = y + dc[i];
-                if(xNew >= 0 and yNew >= 0 and xNew < n and yNew < n and !vis[xNew * n + yNew]){
-                    vis[xNew * n + yNew] = 1;
-                    heap.push({-grid[xNew][yNew],xNew * n + yNew});
+        int n = grid.size();
+        priority_queue<t, vector<t>, comp>pq;
+        pq.push({grid[0][0], 0, 0});
+        vector<int>dr = {0, 0, -1, 1}, dc = {-1, 1, 0, 0};
+        int ans = 0;
+        while(!pq.empty()){
+            auto [val, i, j] = pq.top();
+            ans = max(ans, val);
+            pq.pop();
+            if(i == n - 1 and j == n - 1) break;
+            for(int rc = 0;rc < 4;rc++){
+                int x = i + dr[rc], y = j + dc[rc];
+                if(x >= 0 and y >= 0 and x < n and y < n and grid[x][y] > -1){
+                    pq.push({grid[x][y], x, y});
+                    grid[x][y] = -1;
                 }
             }
         }
+        
         return ans;
     }
 };

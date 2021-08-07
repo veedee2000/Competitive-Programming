64cde8455bcb443321dd74ed0f9dff9a73ce9440
veedee <43088920+veedee2000@users.noreply.github.com>
2021-08-07 11:27:24 +0530

    Update DFS.cpp

--- Leetcode/Detect_Cycle/Directed/DFS.cpp
+++ Leetcode/Detect_Cycle/Directed/DFS.cpp
@@ -1,8 +1,9 @@
-bool dfs(int i, vector<vector<int>>& adj, vector<bool>& vis){
+bool dfs(int i, vector<vector<int>>& adj, vector<bool>& vis, vector<bool>& done){
     if(vis[i]) return 1;
     vis[i] = 1;
+    done[i] = 1;
     for(auto next:adj[i]){
-        if(dfs(next, adj, vis)) return 1;
+        if(dfs(next, adj, vis, done)) return 1;
     }
     vis[i] = 0;
     return 0;
@@ -13,9 +14,9 @@ int Solution::solve(int V, vector<vector<int> > &edgeList) {
     for(auto edge:edgeList){
         adj[edge[0]].push_back(edge[1]);
     }
-    vector<bool>vis(V + 1);
+    vector<bool>vis(V + 1), done(V + 1);
     for(int i = 1;i <= V;i++){
-        if(!vis[i] and dfs(i, adj, vis)) return 1;
+        if(!done[i] and dfs(i, adj, vis, done)) return 1;
     }
     return 0;
 }
